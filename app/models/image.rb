1f491d1bf5386115ab502f5dde4222fafa9b3b27
Murugan <murugu2012@gmail.com>
2016-09-30 23:07:03 +0530

    Direct association between images and likes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,10 @@
 class Image < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :foreign_key => "photo_id",
+             :dependent => :destroy
+
   belongs_to :owner,
              :class_name => "User",
              :counter_cache => :posted_photos_count
